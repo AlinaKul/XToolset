6554d82eef32c35d416025831fa05a0785db6802
AlinaKul <61979859+AlinaKul@users.noreply.github.com>
2021-10-19 11:34:23 +0300

    Revert "Add file fromBuffer"

--- packages/xlsx-import/src/ImporterFactory.ts
+++ packages/xlsx-import/src/ImporterFactory.ts
@@ -1,5 +1,5 @@
-import {Buffer, Workbook} from 'exceljs';
-import { IImporter } from './IImporter';
+import { Workbook } from 'exceljs';
+import IImporterLegacy, { IImporter } from './IImporter';
 import ImporterLegacy, { Importer } from './Importer';
 
 export class ImporterFactory {
@@ -8,14 +8,6 @@ export class ImporterFactory {
         await wb.xlsx.readFile(path);
 
         return new Importer(wb);
-    }
-
-    public async fromBuffer(buffer: Buffer): Promise<IImporter>{
-        const wb = new Workbook();
-        await wb.xlsx.load(buffer);
-
-        return new Importer(wb)
-
     }
 }
 
